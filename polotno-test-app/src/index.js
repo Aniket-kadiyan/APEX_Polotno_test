6ad679400096e69f66a2bfe4c57e4b6e2bb7fabb
Aniket-kadiyan <111952166+Aniket-kadiyan@users.noreply.github.com>
2024-01-17 09:13:04 +0530

    first commit

---
+++ polotno-test-app/src/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { observer } from 'mobx-react-lite';
+
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+
+import { Workspace } from 'polotno/canvas/workspace';
+import { SidePanel } from 'polotno/side-panel';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { createStore } from 'polotno/model/store';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+// import all default sections
+// we will not use all of them in the demo
+// just to show what we have
+import {
+    TextSection,
+    PhotosSection,
+    ElementsSection,
+    UploadSection,
+    BackgroundSection,
+    SizeSection,
+} from 'polotno/side-panel';
+//
+import { SectionTab } from 'polotno/side-panel';
+// import our own icon
+import FaShapes from '@meronex/icons/fa/FaShapes';
+
+const store = createStore({
+    // this is a demo key just for that project
+    // (!) please don't use it in your projects
+    // to create your own API key please go here: https://polotno.com/cabinet
+    key: '8T4ey-lI9-ZrBbS_eml8',
+    // you can hide back-link on a paid license
+    // but it will be good if you can keep it for Polotno project support
+    showCredit: true,
+});
+
+store.addPage();
+store.activePage.addElement({
+    type: 'text',
+    text: 'hello',
+});
+
+// define the new custom section
+const CustomSection = {
+    name: 'custom',
+    Tab: (props) => (
+        <SectionTab name="Custom" {...props}>
+            <FaShapes icon="new-text-box" />
+        </SectionTab>
+    ),
+    // we need observer to update component automatically on any store changes
+    Panel: observer(({ store }) => {
+        return (
+            <div>
+                <p>Here we will define our own custom tab.</p>
+                <p>Elements on the current page: {store.activePage?.children.length}</p>
+                <button id='custom_add_button_test'>add element</button>
+            </div>
+        );
+    }),
+};
+
+function sayHI(){
+    console.log("HI");
+    alert("HI");
+        };
+
+
+
+
+// we will have just two sections
+const sections = [CustomSection, TextSection, BackgroundSection, SizeSection];
+
+export const App = () => {
+    return (
+        <PolotnoContainer className="polotno-app-container">
+            <SidePanelWrap>
+                <SidePanel store={store} sections={sections} defaultSection="custom" />
+            </SidePanelWrap>
+            <WorkspaceWrap>
+                <Toolbar store={store} downloadButtonEnabled />
+                <Workspace store={store} />
+                <ZoomButtons store={store} />
+            </WorkspaceWrap>
+        </PolotnoContainer>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+const addb = store.activePage
+console.log(addb)
+const addbb = store.activePage.children
+console.log(addbb)
+// addb.addEventListener("click",sayHI)
